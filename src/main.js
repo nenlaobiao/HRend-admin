e3ffe771247970d127003e21bfa4695230562db8
Ming <c2083117697@qq.com>
2022-07-13 11:52:25 +0800

    项目初始化

---
+++ src/main.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+// 引入第三方库、让不同浏览器渲染时保持样式一致
+import 'normalize.css/normalize.css' // A modern alternative to CSS resets
+
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+// 语言国际化
+import locale from 'element-ui/lib/locale/lang/en' // lang i18n
+// 全局公共样式
+import '@/styles/index.scss' // global css
+
+import App from './App'
+import store from './store'
+import router from './router'
+
+import '@/icons' // icon
+import '@/permission' // permission control
+
+/**
+ * If you don't want to use mock-server
+ * you want to use MockJs for mock api
+ * you can execute: mockXHR()
+ *
+ * Currently MockJs will be used in the production environment,
+ * MockJs 仅在开发时使用、上线前请删除
+ * please remove it before going online ! ! !
+ */
+// if (process.env.NODE_ENV === 'production') {
+//   const { mockXHR } = require('../mock')
+//   mockXHR()
+// }
+
+// set ElementUI lang to EN
+Vue.use(ElementUI, { locale })
+// 如果想要中文版 element-ui，按如下方式声明
+// Vue.use(ElementUI)
+// 控制台生产提示
+Vue.config.productionTip = false
+
+new Vue({
+  el: '#app',
+  router,
+  store,
+  render: h => h(App)
+})
---
+++ src/settings.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+  title: '人力资源后台管理',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   * 设置appmain右侧主题上的header是不是固定定位
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   * 设置侧边栏上面的logo是否显示
+   */
+  sidebarLogo: true
+}
